e56823c9573779c6f970df2bc0a2b39e865f7957
Сергей <https://github.com>
2019-05-07 23:26:08 +0300

    Class 10

---
+++ src/lesson10/zad4/Dress.java
@@ -0,0 +1,7 @@
+package lesson10.zad4;
+
+public abstract class Dress {
+    private int size;
+    private int price;
+    private String color;
+}
---
+++ src/lesson10/zad4/MenDress.java
@@ -0,0 +1,4 @@
+package lesson10.zad4;
+
+public interface MenDress {
+}
