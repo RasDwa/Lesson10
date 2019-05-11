45d5e563737f2a4ee286796e5dbcef4d47505341
Сергей <https://github.com>
2019-05-11 16:46:24 +0300

    Завершил

--- src/internet/Maine.java
+++ src/internet/Maine.java
@@ -25,27 +25,30 @@ public class Maine {
         User user2 = new User("login2", "parol2", basket2);
 
         Scanner scanner = new Scanner(System.in);
-        /*System.out.println("Введите логин...");
+        System.out.println("Введите логин...");
         String login = scanner.nextLine();
         System.out.println("Введите пароль...");
         String password = scanner.nextLine();
-        System.out.println("Добро пожаловать в наш магазин! \nВыберите цифру товара что вас интересует...");*/
+        System.out.println("Добро пожаловать в наш магазин! "+login+"\nВыберите цифру товара что вас интересует...");
+
+        Tovar[] kyplenueTovaru = new Tovar[2];
         SpisocKataloga[] spisocKatalogas = SpisocKataloga.values();
         for (SpisocKataloga s : spisocKatalogas) {
             System.out.println(s + " -> " + s.ordinal());
         }
         int vibor = scanner.nextInt();
-        Tovar[] kyplenueTovaru = new Tovar[1];
+
         System.out.println("Выберите то что хотите купить...");
         switch (vibor) {
             case 0:
                 for (Tovar t : tovarElectronica) {
-                    System.out.println(t.getName() + " " + t.getPrice() + " " + t.getReyting());
+                    System.out.println(t.getName() + " " + t.getPrice() + " грн " + t.getReyting()+" рейнтинг");
                 }
                 int viborTovaraElectronici = scanner.nextInt();
                 switch (viborTovaraElectronici) {
                     case 1:
                         kyplenueTovaru[0] = new Tovar("Телефон", 3500, 8.7);
+
                         break;
                     case 2:
                         kyplenueTovaru[0] = new Tovar("Фитнес-Браслет", 250, 8.3);
@@ -61,7 +64,7 @@ public class Maine {
                 break;
             case 1:
                 for (Tovar t : tovarUslygi) {
-                    System.out.println(t.getName() + " " + t.getPrice() + " " + t.getReyting());
+                    System.out.println(t.getName() + " " + t.getPrice() + " грн " + t.getReyting()+" рейнтинг");
                 }
                 int viborTovaraUslugi = scanner.nextInt();
                 switch (viborTovaraUslugi) {
@@ -80,8 +83,90 @@ public class Maine {
             default:
                 System.out.println("Не правильная группа");
         }
-        for (Tovar t : kyplenueTovaru) {
-            System.out.println("Вы покупаете: " + t.getName() + " " + t.getPrice() + " " + t.getReyting());
+
+        System.out.println("Желаете продолжить список покупок?\n" +
+                "1. Да\n" +
+                "2. Нет (Показать мою корзину покупок)");
+        int vtoroyEtap = scanner.nextInt();
+        switch (vtoroyEtap) {
+            case 1:
+                for (SpisocKataloga s : spisocKatalogas) {
+                    System.out.println(s + " -> " + s.ordinal());
+                }
+                int vibor2 = scanner.nextInt();
+
+                System.out.println("Выберите то что хотите купить...");
+                switch (vibor2) {
+                    case 0:
+                        for (Tovar t : tovarElectronica) {
+                            System.out.println(t.getName() + " " + t.getPrice() + " грн " + t.getReyting()+" рейнтинг");
+                        }
+                        int viborTovaraElectronici = scanner.nextInt();
+                        switch (viborTovaraElectronici) {
+                            case 1:
+                                kyplenueTovaru[1] = new Tovar("Телефон", 3500, 8.7);
+
+                                break;
+                            case 2:
+                                kyplenueTovaru[1] = new Tovar("Фитнес-Браслет", 250, 8.3);
+                                break;
+                            case 3:
+                                kyplenueTovaru[1] = new Tovar("Зарядка", 310, 7.0);
+                                break;
+                            default:
+                                System.out.println("Введенная не правильная группа");
+                                ;
+                                break;
+                        }
+                        break;
+                    case 1:
+                        for (Tovar t : tovarUslygi) {
+                            System.out.println(t.getName() + " " + t.getPrice() + " грн " + t.getReyting()+" рейнтинг");
+                        }
+                        int viborTovaraUslugi = scanner.nextInt();
+                        switch (viborTovaraUslugi) {
+                            case 1:
+                                kyplenueTovaru[1] = new Tovar("Наклейка пленки", 150, 9.0);
+                                break;
+                            case 2:
+                                kyplenueTovaru[1] = new Tovar("Настройка", 50, 9.1);
+                                break;
+                            default:
+                                System.out.println("Введенная не правильная группа");
+                                ;
+                                break;
+                        }
+                        break;
+                    default:
+                        System.out.println("Не правильная группа");
+                }
+
+                System.out.println("Желаете продолжить список покупок?\n" +
+                        "1. Да\n" +
+                        "2. Нет (Показать мою корзину покупок)");
+                int vibor3 = scanner.nextInt();
+                switch (vibor3){
+                    case 1:
+                        System.out.println("Хватить все покупать...вот то что ты покупаешь: ");
+                        for (Tovar t : kyplenueTovaru) {
+                            System.out.println("Вы покупаете: " + t.getName() + " " + t.getPrice() + " грн " + t.getReyting()+" рейнтинг");
+                        }
+                        break;
+                    case 2:
+                        for (Tovar t : kyplenueTovaru) {
+                            System.out.println("Вы покупаете: " + t.getName() + " " + t.getPrice() + " грн " + t.getReyting()+" рейнтинг");
+                        }
+                        break;
+                }
+                break;
+            case 2:
+                Tovar [] kyplenoOdin = new Tovar[1];
+                System.arraycopy(kyplenueTovaru,0,kyplenoOdin,0,1);
+                for (Tovar t : kyplenoOdin) {
+                    System.out.println("Вы покупаете: " + t.getName() + " " + t.getPrice() + " грн " + t.getReyting()+" рейнтинг");
+                }
+                break;
         }
+
     }
 }
